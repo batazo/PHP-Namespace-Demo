d37680f08d3d5885c5a374a8ae6eb7ed3ac57616
Bata Zoltán <batazo.dad@gmail.com>
2024-10-15 02:04:30 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,5 +3,7 @@
 require_once '../autoload.php';
 
 use App\Controller\TestController;
+use App\Service\XyService;
 
-echo TestController::Hello();
+echo TestController::Hello() . " ";
+echo XyService::exampleService();
